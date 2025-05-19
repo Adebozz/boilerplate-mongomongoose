b0234f82605ed7c26ca951d3859d51738ad2e81f
Adebozz <ademolaadeigbe3@gmail.com>
2025-05-19 15:44:18 +0100

    `Refactor findAndUpdate, removeById, removeManyPeople, and queryChain functions to use Mongoose callbacks`

--- myApp.js
+++ myApp.js
@@ -86,20 +86,35 @@ const findAndUpdate = (personName, done) => {
 
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
+
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })       // sort by name ascending
+    .limit(2)                // limit to 2 results
+    .select("-age")          // exclude the age field
+    .exec((err, data) => {
+      if (err) return done(err);
+      done(null, data);
+    });
 };
+
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
