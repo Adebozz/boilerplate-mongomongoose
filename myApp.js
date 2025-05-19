e4253a25f3b98a97a45dd157d74d36b6c16331d5
Adebozz <ademolaadeigbe3@gmail.com>
2025-05-19 15:23:00 +0100

    `Added database queries to find people by name, favorite food, and find person by ID`

--- myApp.js
+++ myApp.js
@@ -35,12 +35,19 @@ const createManyPeople = (arrayOfPeople, done) => {
 
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
+
 
 const findPersonById = (personId, done) => {
   done(null /*, data*/);
